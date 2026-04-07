dc1896911271b953333f5d9179b63e7080eaeff7
moroninavarro <moronialanis23@gmail.com>
2026-04-06 19:08:04 -0700

    Updating classes again

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        instructions();
-        game();
+        Game game = new Game();
+        game.instructions();
+        game.play();
 
     }
+}
 
 
 
-    public static void instructions(){
+
+class Game{
+
+    //My function for the basic instructions for the game.
+    public void instructions(){
         System.out.println("Welcome to the Guess the Number Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
 
@@ -22,7 +27,8 @@ public class Main {
     }
 
 
-    public static void game(){
+
+    public void play(){
         Random rand = new Random();
         Scanner scanner = new Scanner (System.in);
         int number = rand.nextInt(100) + 1;
@@ -32,7 +38,8 @@ public class Main {
         int maxAttempts = 10;
 
 
-
+//Creating the loop to handle the attempts and maxAttempts of the user
+//This will display the messages for the user.
         while (attempts < maxAttempts){
 
             System.out.print("Enter your guess: ");
@@ -47,7 +54,7 @@ public class Main {
 
             } else if (guess == number) {
                 System.out.println(" ");
-                System.out.println("Congratulations, you guessed the number in "+ attempts + " " + "attempts.");
+                System.out.println("🎉 Congratulations! You guessed the number in "+ attempts + " " + "attempts.");
                 break;
             }
             else if (guess > number){
@@ -55,23 +62,19 @@ public class Main {
                 System.out.println("Too high! Guess again.");
             }
 
-
-
-
-
             if(attempts < maxAttempts){
                 System.out.println(" ");
-                System.out.println("Te quedan " + (maxAttempts - attempts) + " intentos.");
+                System.out.println("You have " + (maxAttempts - attempts) + " attempts left.");
             }
             else{
-                System.out.println("Se acabaron tus intentos, el numero era " + number );
+                System.out.println("Game Over! The number was " + number );
             }
         }
 
 
 
-
-
     }
+
+
 
 }
